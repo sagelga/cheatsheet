03046d965b8769ebc3a60a35c7097d7bb2108306
Kunanon Srisuntiroj <son9912@gmail.com>
2022-06-28 08:14:25 +0000

    Migrate git sidebar to TS

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -1,3 +1,52 @@
 import type { SidebarConfig } from "@vuepress/theme-default";
 
-export const en: SidebarConfig = {};
+export const en: SidebarConfig = {
+  "/git/": [
+    {
+      text: "Getting Started",
+      link: "/git/initiate/",
+      children: [
+        "/git/initiate/install/",
+        "/git/initiate/repository/",
+        "/git/initiate/origin-setup/",
+      ],
+    },
+    {
+      text: "Commit",
+      link: "/git/commit/",
+      children: [
+        {
+          text: "Unstaged",
+          children: ["/git/commit/unstage/add/", "/git/commit/unstage/reset/"],
+        },
+        {
+          text: "Staged",
+          children: ["/git/commit/stage/commit/", "/git/commit/stage/remove/"],
+        },
+      ],
+    },
+    {
+      text: "Branching",
+      children: [
+        "/git/branch/",
+        "/git/branch/workflow/",
+        "/git/branch/create/",
+        "/git/branch/diff/",
+      ],
+    },
+    {
+      text: "Local / Remote",
+      children: ["/git/origin/pull/", "/git/origin/push/"],
+    },
+    {
+      text: "Merge Strategies",
+      link: "/git/merge/",
+      children: [
+        "/git/merge/recursive/",
+        "/git/merge/rebase/",
+        "/git/merge/squash/",
+        "/git/merge/conflict/",
+      ],
+    },
+  ],
+};
