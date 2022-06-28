f30dd336e3792b78463a3b5cc1cba0b070cdd789
Kunanon Srisuntiroj <son9912@gmail.com>
2022-06-28 08:17:51 +0000

    Migrate sql sidebar to TS

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -99,6 +99,75 @@ export const en: SidebarConfig = {
       ],
     },
   ],
+  "/sql/": [
+    {
+      text: "Getting Started",
+      children: ["/sql/intro/command/", "/sql/table/data-type/"],
+    },
+    {
+      text: "Querying data",
+      children: ["/sql/view/select/", "/sql/view/where/"],
+    },
+    {
+      text: "Database Tables",
+      children: [
+        "/sql/table/constraint/",
+        "/sql/table/create/",
+        "/sql/table/describe/",
+        "/sql/table/rename/",
+        "/sql/table/drop/",
+      ],
+    },
+    {
+      text: "Update Data",
+      children: ["/sql/data/insert/", "/sql/data/delete/", "/sql/data/update/"],
+    },
+    {
+      text: "Edit Tables",
+      children: [
+        "/sql/table/alter/add/",
+        "/sql/table/alter/modify/",
+        "/sql/table/alter/drop/",
+      ],
+    },
+    {
+      text: "Grouping",
+      children: ["/sql/view/group/", "/sql/view/group/function/"],
+    },
+    {
+      text: "Joining Tables",
+      children: [
+        "/sql/table/relationship/",
+        "/sql/view/join/inner/",
+        "/sql/view/join/inner/CartesianProduct/",
+        "/sql/view/join/inner/Cross/",
+        "/sql/view/join/inner/Equijoin/",
+        "/sql/view/join/inner/Natural/",
+        "/sql/view/join/outer/left/",
+        "/sql/view/join/outer/right/",
+        "/sql/view/join/outer/full/",
+      ],
+    },
+    {
+      text: "Transaction control",
+      children: [
+        "/sql/table/commit/",
+        "/sql/table/rollback/",
+        "/sql/table/savepoint/",
+      ],
+    },
+    {
+      text: "Normalization",
+      children: [
+        "/sql/table/normalize/1NF.html",
+        "/sql/table/normalize/2NF.html",
+        "/sql/table/normalize/3NF.html",
+        "/sql/table/normalize/4NF.html",
+        "/sql/table/normalize/Boyce-CoddNF.html",
+        "/sql/table/normalize/5NF.html",
+      ],
+    },
+  ],
   "/git/": [
     {
       text: "Getting Started",
