a95621d9c0ec53c46f16ff92f4ef502e472d2589
Kunanon Srisuntiroj <son9912@gmail.com>
2022-06-28 08:18:47 +0000

    Migrate spss sidebar to TS

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -216,4 +216,24 @@ export const en: SidebarConfig = {
       ],
     },
   ],
+  "/spss/": [
+    {
+      text: "Data Preparation",
+      children: ["/spss/data/clean/", "/spss/data/transform/"],
+    },
+    {
+      text: "Create Model",
+      children: [
+        "/spss/model/anova/two-way/",
+        "/spss/model/linear-regression/",
+        "/spss/model/linear-regression/multiple/",
+        "/spss/model/logistic-regression/",
+        "/spss/model/time-series/",
+      ],
+    },
+    {
+      text: "Tests",
+      children: ["/spss/test/assumption/"],
+    },
+  ],
 };
